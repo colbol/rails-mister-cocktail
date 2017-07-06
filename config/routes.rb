3113e9f99a435581f08f10b6adfa701adafed1c0
Matthew Collis <matthew.collis@mail.mcgill.ca>
2017-07-06 16:01:32 -0400

    asdasD

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,21 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  # get 'cocktails/index'
+
+  # get 'cocktails/show'
+
+  # get 'cocktails/new'
+
+  # get 'cocktails/create'
+
+  # get 'doses/new'
+
+  # get 'doses/create'
+
+  # get 'destroy', to: 'doses#destroy'
+
+  resources :cocktails do
+    resources :doses, only: [:new, :create]
+  end
+  resources :doses, only: [:index, :show, :edit, :new, :destroy]
 end
